b7ee2ca0ea9e29e8ee30593e905decaa38b2c797
lanhao945 <bigpangl@163.com>
2023-06-25 14:33:34 +0800

    refactor(APIS): change by docs
    Signed-off-by: lanhao945 <bigpangl@163.com>

--- sfm/core.py
+++ sfm/core.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Time      :2023/6/25 10:09
+# Time      :2023/6/25 14:29
 # Author    :LanHao
 # ▄▄▄█████▓ █    ██  ██ ▄█▀▓█████ ▓█████▄
 # ▓  ██▒ ▓▒ ██  ▓██▒ ██▄█▒ ▓█   ▀ ▒██▀ ██▌
@@ -12,57 +12,17 @@
 #             ░     ░  ░      ░  ░   ░
 #                                  ░
 
-import abc
-import logging
-from typing import Any, Iterator
+"""
 
-import numpy as np
-import numpy.typing as npt
-from attr import attrs, attrib
+在改模块下重构revise_v2 中的代码
+
+"""
+
+import logging
+
+from .datas import SFMData, ColorPoints
 
 logger = logging.getLogger(__name__)
-
-_DEFAULT_K = np.array([
-    [2362.12, 0, 720],
-    [0, 2362.12, 578],
-    [0, 0, 1]])
-
-
-@attrs
-class Camera:
-    mrt = attrib(type=float, default=0.7)
-    k = attrib(type=npt.NDArray[np.float], default=_DEFAULT_K)
-    x = attrib(type=float, default=0.5)
-    y = attrib(type=int, default=1)
-
-
-class ImageDataset(abc.ABC):
-    @abc.abstractmethod
-    def __len__(self) -> int:
-        ...
-
-    @abc.abstractmethod
-    def __getitem__(self, item) -> npt.NDArray[np.uint8]:
-        ...
-
-    @abc.abstractmethod
-    def __iter__(self) -> Iterator[npt.NDArray[np.uint8]]:
-        ...
-
-
-_DEFAULT_CAMERA = Camera()
-
-
-@attrs
-class SFMData:
-    image = attrib(type=ImageDataset)
-    camera = attrib(type=Camera, default=_DEFAULT_CAMERA)
-
-
-@attrs
-class ColorPoints:
-    points = attrib(type=Any)
-    colors = attrib(type=Any)
 
 
 def rebuild(sfm_data: SFMData) -> ColorPoints:
