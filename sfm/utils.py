c914dc3e1651b54e2a7cb460c57bd690d7bdb991
lanhao945 <bigpangl@163.com>
2023-06-25 11:30:52 +0800

    feat(sfm package): 初次,按文档实现外层约定
    Signed-off-by: lanhao945 <bigpangl@163.com>

---
+++ sfm/utils.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+# Time      :2023/6/25 10:37
+# Author    :LanHao
+# ▄▄▄█████▓ █    ██  ██ ▄█▀▓█████ ▓█████▄
+# ▓  ██▒ ▓▒ ██  ▓██▒ ██▄█▒ ▓█   ▀ ▒██▀ ██▌
+# ▒ ▓██░ ▒░▓██  ▒██░▓███▄░ ▒███   ░██   █▌
+# ░ ▓██▓ ░ ▓▓█  ░██░▓██ █▄ ▒▓█  ▄ ░▓█▄   ▌
+#   ▒██▒ ░ ▒▒█████▓ ▒██▒ █▄░▒████▒░▒████▓
+#   ▒ ░░   ░▒▓▒ ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░ ▒▒▓  ▒
+#     ░    ░░▒░ ░ ░ ░ ░▒ ▒░ ░ ░  ░ ░ ▒  ▒
+#   ░       ░░░ ░ ░ ░ ░░ ░    ░    ░ ░  ░
+#             ░     ░  ░      ░  ░   ░
+#                                  ░
+
+import os
+from typing import List
+
+import cv2
+
+from .core import ImageDataset
+
+
+class LocalStorageImageDataset(ImageDataset):
+    """
+    磁盘目录载入数据
+    """
+
+    _data_path: str
+    _img_names: List[str]
+
+    def __init__(self, data_path):
+        img_names = os.listdir(data_path)
+        self._img_names = sorted(img_names)
+        self._data_path = data_path
+
+    def __getitem__(self, item):
+        image_path = os.path.join(
+            self._data_path,
+            self._img_names[item]
+        )
+        if not os.path.exists(image_path):
+            raise Exception(f"{image_path} not exists")
+        image = cv2.imread(image_path)
+        return image
+
+    def __len__(self):
+        return len(self._img_names)
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+
+if __name__ == '__main__':
+    pass
